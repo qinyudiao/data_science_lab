08c13726615a4d4d27f0499f2e796dac78f0f6d5
qinyudiao <qdiao@utexas.edu>
2020-02-21 22:29:41 -0600

    generate random paragraph

--- Lab3/calculateEntropy.py
+++ Lab3/calculateEntropy.py
@@ -1,4 +1,5 @@
 from scipy.stats import entropy
+from random import random
 
 def sort_by_frequency(alist): 
     dict = {} 
@@ -20,9 +21,26 @@ with open('listOfPDFWords.txt', 'r') as filehandle:
         words.append(currentWord)
 
 num_words = len(words)
+print(num_words)
 reverse_words = sort_by_frequency(words)
 list_of_frequency = []
+list_word_freq_pair = []
+
+total_probrability = 0
 for word_freq_pair in reverse_words:
     list_of_frequency.append(word_freq_pair[1]/num_words)
+    total_probrability += word_freq_pair[1]/num_words
+    list_word_freq_pair.append([word_freq_pair[0], total_probrability])
 
 print("entropy: " + str(entropy(list_of_frequency, base=num_words)))
+
+paragraph = ""
+for i in range(0, 100):
+    num_rand = random()
+    index = 0
+    while (num_rand>list_word_freq_pair[index][1]):
+        index += 1
+    paragraph += list_word_freq_pair[index][0]
+    paragraph += " "
+
+print(paragraph)
